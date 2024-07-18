c23e1268929981793ecfef91edd7226361151941
harlanpierre <harlan.evron@gmail.com>
2024-07-17 22:18:00 -0300

    feat: First commit

---
+++ src/main/java/br/com/hsiphones/iphoneswapcenterbackend/IphoneSwapCenterBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.hsiphones.iphoneswapcenterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IphoneSwapCenterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IphoneSwapCenterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hsiphones/iphoneswapcenterbackend/IphoneSwapCenterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hsiphones.iphoneswapcenterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IphoneSwapCenterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
